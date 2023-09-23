e3c72edb3384eaf0f96d96cb72e4ae6c1db420d6
eams1798 <eams1798@gmail.com>
2023-09-23 02:41:29 -0400

    advanced hooks - useCountry

---
+++ country-hook/src/hooks/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(({data}) => {
+        setCountry({ data, found: true })
+      })
+      .catch(error => {
+        setCountry({ found: false })
+      })
+  }, [name])
+
+
+  return country
+}
